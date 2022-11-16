dc599aa9b552ebb0a526f956589a22ba5fe2d4dd
Stefan <stefan.dankuc.dev@gmail.com>
2022-11-16 16:21:01 +0100

    "book class created"

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+# class Book which inherits from class Model (build in class provided by Django)
+class Book(models.Model):
+    # attribute for title is indicator which data we want to store for attribute, charField = small to large strings
+    #check Fieldtypes in documentation
+    title = models.CharField(max_length=50) # required argument max_lenght
+    rating = models.IntegerField() # no required argument
+    
